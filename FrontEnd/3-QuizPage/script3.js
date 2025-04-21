3c0c49e642b349317067700971067d061e03e21e
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-04-21 13:50:10 +0300

    adding EventListener.txt

---
+++ FrontEnd/3-QuizPage/script3.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const quizTitle = document.getElementById("quiz-title");
+    const quizForm = document.getElementById("quiz-form");
+    const submitBtn = document.getElementById("submit-btn");
+    const resultBox = document.getElementById("result");
+});
