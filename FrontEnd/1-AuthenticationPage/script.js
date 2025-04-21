c72ea04397edef0dc7ab356172565617fac054b0
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-04-21 12:22:37 +0300

    selecting elements.txt

---
+++ FrontEnd/1-AuthenticationPage/script.js
@@ -0,0 +1,4 @@
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
+const loginTab = document.getElementById("login-tab");
+const registerTab = document.getElementById("register-tab");
