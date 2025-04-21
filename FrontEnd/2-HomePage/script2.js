345ef576719ab12e787b5e055e7e69995ad0cf81
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-04-21 12:49:55 +0300

    show welcome message.txt

--- FrontEnd/2-HomePage/script2.js
+++ FrontEnd/2-HomePage/script2.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const quizListElement = document.getElementById("quiz-list");
-    const welcomeMsg = document.getElementById("welcome-msg");
+    const welcomemessage = document.getElementById("welcome-msg");
     const user = JSON.parse(localStorage.getItem("currentUser"));
     if (!user) {
       alert("Please login first.");
       window.location.href = "index.html";
       return;
     }
+    welcomemessage.textContent = `Hello, ${user.email}! Ready to take a quiz?`;
 });
