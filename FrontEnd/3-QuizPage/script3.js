183ad62954714cb35eb65ea340b7980e7973a951
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-04-21 13:08:55 +0300

    editing files.txt

---
+++ FrontEnd/3-QuizPage/script3.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const quizListElement = document.getElementById("quiz-list");
+    const welcomemessage = document.getElementById("welcome-msg");
+    const user = JSON.parse(localStorage.getItem("currentUser"));
+    if (!user) {
+      alert("Please login first.");
+      window.location.href = "index.html";
+      return;
+    }
+    welcomemessage.textContent = `Hello, ${user.email}! Ready to take a quiz?`;
+    const quizzes = JSON.parse(localStorage.getItem("quizzes") || "[]");
+    if (quizzes.length === 0) {
+      quizListElement.innerHTML = "<li>No quizzes available.</li>";
+      return;
+    }
+    quizzes.forEach((quiz) => {
+        const li = document.createElement("li");
+        li.textContent = quiz.title;
+        li.onclick = () => {
+          localStorage.setItem("currentQuiz", JSON.stringify(quiz));
+          window.location.href = "take-quiz.html";
+        };
+        quizListElement.appendChild(li);
+      });
+});
