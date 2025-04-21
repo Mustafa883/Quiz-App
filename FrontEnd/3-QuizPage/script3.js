3292994ed4f53dd15ffd07ffca2e6078f6cc048f
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-04-21 13:50:55 +0300

    add user.txt

--- FrontEnd/3-QuizPage/script3.js
+++ FrontEnd/3-QuizPage/script3.js
@@ -3,4 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const quizForm = document.getElementById("quiz-form");
     const submitBtn = document.getElementById("submit-btn");
     const resultBox = document.getElementById("result");
+    const user = JSON.parse(localStorage.getItem("currentUser"));
+    if (!user) {
+      alert("Please login first.");
+      window.location.href = "index.html";
+      return;
+    }
 });
