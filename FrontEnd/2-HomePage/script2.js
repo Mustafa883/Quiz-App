439870bae9fcb0772467feb2625977e407dc3065
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-04-21 12:49:04 +0300

    check login.txt

--- FrontEnd/2-HomePage/script2.js
+++ FrontEnd/2-HomePage/script2.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-}
+    const quizListElement = document.getElementById("quiz-list");
+    const welcomeMsg = document.getElementById("welcome-msg");
+    const user = JSON.parse(localStorage.getItem("currentUser"));
+    if (!user) {
+      alert("Please login first.");
+      window.location.href = "index.html";
+      return;
+    }
+});
