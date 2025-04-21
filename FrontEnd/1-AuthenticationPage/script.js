ab26b59a09bf741bfd438926142ba0faa3678b0b
Mustafa Al-Omari <mustafaalomari@gmail.com>
2025-04-21 12:24:32 +0300

    loginTab.txt

--- FrontEnd/1-AuthenticationPage/script.js
+++ FrontEnd/1-AuthenticationPage/script.js
@@ -2,3 +2,10 @@ const loginForm = document.getElementById("login-form");
 const registerForm = document.getElementById("register-form");
 const loginTab = document.getElementById("login-tab");
 const registerTab = document.getElementById("register-tab");
+loginTab.onclick = () => {
+    loginForm.classList.remove("hidden");
+    registerForm.classList.add("hidden");
+    loginTab.classList.add("active");
+    registerTab.classList.remove("active");
+  };
+  
